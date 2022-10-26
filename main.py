a28e9d2fac6d591f1ed9d72d653c40f06c1164ed
Raphael Castro <rafaphcastro@gmail.com>
2022-10-26 01:24:14 -0300

    chore: start project

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+from sanic import Sanic
+from sanic.response import text
+
+app = Sanic("MyHelloWorldApp")
+
+@app.get("/")
+async def hello_world(request):
+    return text("Hello, world.")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8000)), fast=True)
